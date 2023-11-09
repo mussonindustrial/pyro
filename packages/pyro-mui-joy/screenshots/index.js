0640b65639bce3a4fb8cb26a31d51f0b8ee658ff
benmusson <ben.d.musson@gmail.com>
2023-11-09 16:11:29 -0500

    linting

--- packages/pyro-mui-joy/screenshots/index.js
+++ packages/pyro-mui-joy/screenshots/index.js
@@ -17,26 +17,24 @@ function cleanScreenshots() {
 }
 
 async function build(theme) {
-
-    const tests = await glob("screenshots/__tests__/**/*.html")
+    const tests = await glob('screenshots/__tests__/**/*.html')
     console.log(`Generating ${tests.length} screenshots for theme ${theme}`)
 
     const browser = await puppeteer.launch({ headless: 'new' })
     const page = await browser.newPage()
 
     const takeScreenshot = async function (testPath) {
-
         var testSource = testPath.split(path.sep)
         testSource.shift()
         testSource = path.join(...testSource)
         const testFile = `file://${__dirname}/${testSource}`
 
-        const screenshotPath = path.format(
-            { ...path.parse(testPath.replace('__tests__', `output/${theme}`)),
-                base: '',
-                ext: '.png' }
-            )
-        
+        const screenshotPath = path.format({
+            ...path.parse(testPath.replace('__tests__', `output/${theme}`)),
+            base: '',
+            ext: '.png',
+        })
+
         const folder = screenshotPath.split(path.sep)
         folder.pop()
         const folderPath = path.join(...folder)
@@ -44,13 +42,17 @@ async function build(theme) {
         if (!fs.existsSync(folderPath)) {
             fs.mkdirSync(folderPath, { recursive: true })
         }
- 
+
         await page.goto(testFile)
         await page.addStyleTag({ path: perspectiveComponents })
         await page.addStyleTag({ path: perspectiveClient })
         await page.addStyleTag({ path: themePath(theme) })
 
-        await page.setViewport({width: 1280, height: 1024, deviceScaleFactor: 2})
+        await page.setViewport({
+            width: 1280,
+            height: 1024,
+            deviceScaleFactor: 2,
+        })
         await page.screenshot({
             path: screenshotPath,
         })
