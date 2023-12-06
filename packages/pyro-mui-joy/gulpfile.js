ddad16a9c5bc0a99dc55bfe82779925cc9336c86
benmusson <ben.d.musson@gmail.com>
2023-12-05 22:51:41 -0500

    docs(changeset): added development server functionality

--- packages/pyro-mui-joy/gulpfile.js
+++ packages/pyro-mui-joy/gulpfile.js
@@ -2,45 +2,86 @@ var postcss = require('gulp-postcss')
 var gulp = require('gulp')
 var path = require('path')
 var fs = require('fs')
-var screenshots = require('./screenshots')
+var { createGateway } = require('./dev/setup')
+const {
+    uploadFonts,
+    uploadThemes,
+    uploadProjectImport,
+    requestScan,
+    refreshSessions,
+} = require('./dev/upload')
 
 const output = './output'
 const source = './src'
+let gateway
 
 gulp.task('clean', function () {
     fs.rmSync(output, { recursive: true, force: true })
-    screenshots.cleanScreenshots()
     return gulp.src(source)
 })
-
-function copyFont(font) {
-    fs.copyFile(
-        path.join(source, 'fonts', font),
-        path.join(output, font),
-        (err) => {
-            if (err) {
-                console.log('Error copying font: ', err)
-            }
-        }
-    )
-}
 
 gulp.task('build', function () {
-    if (!fs.existsSync(output)) {
-        fs.mkdirSync(output, { recursive: true })
-    }
+    fs.cpSync(
+        path.join('.', source, 'fonts'),
+        path.join('.', output, 'fonts'),
+        {
+            recursive: true,
+        }
+    )
 
-    copyFont('Inter-italic.var.woff2')
-    copyFont('Inter-roman.var.woff2')
-    return gulp.src('./src/*.css').pipe(postcss()).pipe(gulp.dest('./output'))
+    return gulp
+        .src('./src/*.css')
+        .pipe(postcss())
+        .pipe(gulp.dest(path.join(output, 'themes')))
 })
 
-gulp.task('generate-screenshots', async function () {
-    await screenshots.build('joy-light')
-    await screenshots.build('joy-dark')
+// gulp.task('generate-screenshots', async function () {
+//     await screenshots.build('joy-light')
+//     await screenshots.build('joy-dark')
+//     return gulp.src(source)
+// })
+
+gulp.task('dev:start', async function () {
+    console.log('Creating development gateway...')
+    gateway = await createGateway()
+    console.log(`Development gateway started: ${gateway.getURI()}`)
+
+    gulp.series('build', 'dev:upload')
     return gulp.src(source)
 })
 
-gulp.task('all', gulp.series('clean', 'build', 'generate-screenshots'))
+gulp.task('dev:upload', async function () {
+    console.log('Uploading fonts and themes...')
+    await Promise.all([uploadFonts(gateway), uploadThemes(gateway)])
+    // console.log('Uploading fonts...')
+    // await uploadFonts(gateway)
+
+    // console.log('Uploading themes...')
+    // await uploadThemes(gateway)
+
+    // console.log('Uploading project import files...')
+    // await uploadProjectImport(gateway)
+
+    console.log('Requesting project resource scan...')
+    await requestScan(gateway)
+
+    console.log('Refreshing Perspective Sessions...')
+    await refreshSessions(gateway)
+
+    console.log('Upload complete.')
+    return gulp.src(source)
+})
+
+gulp.task('dev:watch', function () {
+    console.log('Watching for source file changes...')
+    gulp.watch(
+        './src/**/*.*',
+        { events: 'all' },
+        gulp.series('clean', 'build', 'dev:upload')
+    )
+})
+
+gulp.task('all', gulp.series('clean', 'build'))
+gulp.task('dev', gulp.series('dev:start', 'dev:upload', 'dev:watch'))
 
 exports.default = gulp
